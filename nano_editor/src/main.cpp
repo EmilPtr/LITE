4b566a8b33ff7251368d1320c0fd5466f8b3323f
LimeTheDerg <emilsahel@gmail.com>
2025-07-15 17:14:01 -0400

    implemented most of the basic text editing functionality

--- nano_editor/src/main.cpp
+++ nano_editor/src/main.cpp
@@ -1,22 +1,20 @@
-#include <iostream>
-#include <ncurses.h>
+#include "lib/helper_functions.h"
+#include "lib/buffer.h"
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in
-// the gutter.
+using std::string;
+
+// Main event loop
 int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how
-    // CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
-
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon
-        // src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint for you, but you can always add more by pressing
-        // <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
+    init_ncurses();
+    std::vector<std::string> buffer = initialize_buffer();
+    int rows,cols;
+    while (true) {
+        clear();
+        getmaxyx(stdscr, rows, cols);
+        print_header(cols);
+        print_buffer(buffer);
+        mvprintw(rows-1, 0, "A silly creation by LimeTheDerg");
+        modify_buffer(getch(), buffer);
+        refresh();
     }
-
-    return 0;
-    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you
-    // can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
 }
